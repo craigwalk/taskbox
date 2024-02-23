16ae742141ae43c7bead9ef7b471cb472c1b8299
craigwalk <105362441+craigwalk@users.noreply.github.com>
2024-02-23 23:23:04 +1000

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,18 @@
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/components/**/*.stories.@(js|jsx)'],
-  staticDirs: ['../public'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-   '@storybook/addon-a11y',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
